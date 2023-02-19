267c8604e2651f036ca3c463c69ac4c6cbdf13d4
MaryanaAm <marydeaddoll@gmail.com>
2023-02-19 20:49:20 +0300

    The tasks from HW #1 was completed

---
+++ HW_1/task_2.py
@@ -0,0 +1,12 @@
+# Задача 4:
+
+sum = int(input('Введите сумму: '))
+print(sum // 6, sum * 4 // 6, sum // 6)
+
+#или
+
+sum2 = int(input('Введите сумму: '))
+k = int(sum2 / 3 * 2) 
+p = int(sum2 / 3 / 2)
+s = p
+print(p, k, s)
